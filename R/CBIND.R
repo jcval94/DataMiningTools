0f0d027ae697c299f4a6f6c4213eb9cab5c0dc96
José Carlos <30965492+jcval94@users.noreply.github.com>
2019-08-20 21:23:58 +0100

    bind

--- R/CBIND.R
+++ R/CBIND.R
@@ -1,4 +1,25 @@
-CBIND <- function(df1, df2) {
+#' Combine two or more data.frame, tibble obects and vectors by column adding new rows and columns if necesary and filling missing values with NA
+#'
+#' @param df1 first data.frame or matrix to be join
+#' @param df2 second data.frame or matrix to be join
+#'
+#' @return New data.frame with merged cells
+#' @export
+#'
+#' @importFrom purrr map
+#' @importFrom tibble as_tibble
+#'
+#' @examples
+#'
+#' data(iris)
+#' d_1 <- iris[, 5]
+#' df2 <- d_1
+#' df1 <- rbind(iris, iris)
+#' df3<-CBIND(df1, df2, new.col=FALSE)
+#' df3
+#' dim(df3)
+#'
+CBIND <- function(df1, df2,...) {
     is_df <- purrr::map_lgl(list(df1, df2), is.data.frame)
     if (any(is_df)) {
         df1 <- tibble::as_tibble(df1)
@@ -7,7 +28,7 @@ CBIND <- function(df1, df2) {
     nd1 <- nrow(df1)
     nd2 <- nrow(df2)
     if (nd1 == nd2) {
-        return(cbind(df1, df2))
+        return(cbind(df1, df2,...))
     }
     else {
         mxm <- c(nd1, nd2) != max(c(nd1, nd2))
@@ -16,15 +37,10 @@ CBIND <- function(df1, df2) {
         df_rw <- c(nd1, nd2)[mxm] - c(nd1, nd2)[!mxm]
         dmx <- rbind(dmx, tibble::as_tibble(matrix(NA, -df_rw, ncol(dmx), dimnames = list(rep(NA, -df_rw), names(dmx)))))
         if (is_df[1]) {
-            return(cbind(dmn, dmx))
+            return(cbind(dmn, dmx,...))
         }
         else {
-            return(cbind(dmx, dmn))
+            return(cbind(dmx, dmn,...))
         }
     }
 }
-data(iris)
-d_1 <- iris[, 5]
-df2 <- d_1
-df1 <- rbind(iris, iris)
-CBIND(df1, df2)
--- R/RBIND.R
+++ R/RBIND.R
@@ -1,3 +1,30 @@
+#' Combine two or more data.frame, tibble obects and vectors by row adding new rows and columns if necesary and filling missing values with NA
+#'
+#' @param df1 first data.frame or matrix to be join
+#' @param df2 second data.frame or matrix to be join
+#'
+#' @return New data.frame with merged cells
+#' @export
+#'
+#'
+#' @importFrom purrr map
+#' @importFrom tibble as_tibble
+#'
+#' @examples
+#'
+#' data(iris)
+#' d_1 <- iris[1, ]
+#' d_1$Species <- as.character(d_1$Species)
+#' d_1[1, 5] <- "Rse"
+#' d_1[["Nueva"]] <- c("U")
+#' ir <- iris
+#' ir$W <- "rr"
+#' tail(RBIND(df1 = ir, df2 = d_1))
+#' head(RBIND(df1 = d_1, df2 = ir))
+#' df3<-RBIND(iris, esoph)
+#' df3
+#' dim(df3)
+#'
 RBIND <- function(df1, df2) {
     nd1 <- ncol(df1)
     nd2 <- ncol(df2)
@@ -20,19 +47,8 @@ RBIND <- function(df1, df2) {
             dmn <- cbind(dmn, tibble::as_tibble(matrix(NA, nrow(dmn), length(max_ex), dimnames = list(1:nrow(dmn), max_ex))))
             dmx <- cbind(dmx, tibble::as_tibble(matrix(NA, nrow(dmx), length(min_ex), dimnames = list(1:nrow(dmx), min_ex))))
         }
-        if (nd1 < nd2 | mxm[1]) 
+        if (nd1 < nd2 | mxm[1])
             return(rbind(dmx, dmn))
         else return(rbind(dmn, dmx))
     }
 }
-data(iris)
-d_1 <- iris[1, ]
-d_1$Species <- as.character(d_1$Species)
-d_1[1, 5] <- "Rse"
-d_1[["Nueva"]] <- c("U")
-ir <- iris
-ir$W <- "rr"
-tail(RBIND(df1 = ir, df2 = d_1))
-head(RBIND(df1 = d_1, df2 = ir))
-library(data)
-RBIND(iris, esoph)
