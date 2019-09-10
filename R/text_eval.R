2b7f33f301b4134990be00fc34ef8dc79b9a26dc
José Carlos <30965492+jcval94@users.noreply.github.com>
2019-09-10 11:04:05 +0100

    description txt ev

--- R/text_eval.R
+++ R/text_eval.R
@@ -1,11 +1,21 @@
-#' Title
+#' Evaluates an string and compile it
 #'
-#' @param a
+#' @param a a string containing R code
 #'
-#' @return
+#' @return the string evaluated
 #' @export
 #'
 #' @examples
+#'
+#' #A sum
+#' text_eval("1+1")
+#'
+#' #Assign random numbers into a variable
+#' set.seed(31109)
+#' text_eval("A<-runif(30);A")
+#'
+#' #Plot an histogram
+#' text_eval("hist(rnorm(30))")
 text_eval <- function(a) {
     if (class(a) != "character") {
         return(invisible())
