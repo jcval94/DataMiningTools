9f862a1ce369702981f8d9316bc92c49be2752dc
José Carlos <30965492+jcval94@users.noreply.github.com>
2019-09-10 11:04:33 +0100

    Parameters included

--- R/TrainTestSplit.R
+++ R/TrainTestSplit.R
@@ -1,19 +1,27 @@
 #' Title
 #'
-#' @param df
-#' @param split
+#' @param df data.frame to be split
+#' @param split percentage of the train chunk
 #'
-#' @return
+#' @return a list and ...
 #' @export
 #'
 #' @examples
+#'
+#' data(iris)
+#' set.seed(31109)
+#' TrainTestSplit(iris)
+#'
+#' head(train);dim(train)
+#'
+#' head(test);dim(test)
+#'
 TrainTestSplit <- function(df, split = 0.75) {
     if (!any("data.frame" %in% class(df))) {
         exit("must be a data.frame object")
     }
-    RndmV <- runif(nrow(df), 0, 1)
-    tr <- RndmV < split
-    list(df[tr, ], df[!tr, ])
+    tr <- sample(1:nrow(df),floor(nrow(df)*split))
+    assign("train",df[tr, ],envir = globalenv())
+    assign("test",df[-tr, ],envir = globalenv())
+    list(df[tr, ], df[-tr, ])
 }
-data(iris)
-A <- TrainTestSplit(iris)
