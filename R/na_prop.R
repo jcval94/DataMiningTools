6b16bb677952220cab1f93eb1b1f044215cdf926
José Carlos <30965492+jcval94@users.noreply.github.com>
2019-09-27 18:25:11 +0100

    final function

--- R/na_prop.R
+++ R/na_prop.R
@@ -1,12 +1,18 @@
-#' Title
+#' Shows the proportion of NA's in a vector
 #'
-#' @param X
+#' @param X a numeric vector
 #'
-#' @return
+#' @return proportion of NA's in a vector
 #' @export
 #'
 #' @examples
+#'
+#' na_prop(c(NA, rnorm(100)))
+#'
 na_prop <- function(X) {
+  if(!is.numeric(X)){
+    warning("X must contain numeric values");return(invisible)
+  }
     sum(ifelse(is.na(X), 1, 0))/length(X)
 }
-na_prop(c(NA, rnorm(100)))
+
