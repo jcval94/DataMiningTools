1af4d8f157179c5871a5d0bd0361b8dec53c470c
José Carlos <30965492+jcval94@users.noreply.github.com>
2019-09-30 23:46:33 +0100

    description

--- R/%c%.R
+++ R/%c%.R
@@ -1,12 +1,19 @@
-#' content operator in a numeric range
+#' determines if a number is between two nombers given
 #'
-#' @param e1
-#' @param e2
+#' @param e1 numeric vector
+#' @param e2 numeric range
 #'
-#' @return
+#' @return TRUE or FALSE vector
 #' @export
 #'
 #' @examples
+#'
+#' 100 %c% c(93,124)
+#'
+#' !runif(100) %c% c(0,1)
+#'
+#'
+#'
 `%c%` <- function(e1, e2){
   as.numeric(e1)>=as.numeric(e2[1]) & as.numeric(e1)<=as.numeric(e2[2])
 }
