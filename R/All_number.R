52f47cc7e37743f63e55411c0857e2bba0c78e10
José Carlos <30965492+jcval94@users.noreply.github.com>
2019-10-27 20:56:20 +0000

    Sapply change

--- R/All_number.R
+++ R/All_number.R
@@ -1,12 +1,13 @@
-#' Transform all non numeric variables of a data.frame into numeric variables by geting dummies features of each variable category
+#' Transforms all non numeric variables of a data.frame into numeric variables by geting dummies features of each variable category
 #'
 #' @param df data.frame object
 #'
 #' @return data.frame object with new variables
 #' @export
 #'
-#' @importFrom purrr map
+#'
 #' @importFrom dplyr as_tibble
+#' @importFrom DataMinigTools categorizar
 #'
 #' @examples
 #'
@@ -15,8 +16,8 @@
 #' Iris
 #'
 all_number <- function(df,...) {
-    df <- as_tibble(df)
-    classes <- purrr::map_chr(df, class)
+    df <- dplyr::as_tibble(df)
+    classes <- sapply(df, class)
     nnm <- !classes %in% "numeric"
     no_num <- df[, names(df)[nnm]]
     no_mun_2 <- categorizar(no_num, ALL = F,...)
