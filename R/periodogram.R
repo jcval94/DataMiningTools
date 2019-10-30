71845a5832e22d4aa196459a430c562c2da7ed12
José Carlos <30965492+jcval94@users.noreply.github.com>
2019-10-30 22:30:35 +0000

    Argument

--- R/periodogram.R
+++ R/periodogram.R
@@ -11,24 +11,25 @@
 #'
 #' @examples
 #'
-#' library(timeSeries)
+#' #library(timeSeries)
+#' #data(LPP2005REC)
 #'
-#' data(LPP2005REC)
+#' df <- USArrests
 #'
-#' df <- LPP2005REC
-#'
-#' periodicidad(df, place = 10)
+#' periodogram(df, place = 10)
 #'
 #'
-periodicidad <- function(ts, place = 10) {
+periodogram <- function(ts, place = 10) {
     ddT <- data.frame(freq = c(), spec = c(), orden = c())
     ords <- floor(length(ts) * 0.7):length(ts)
     for (lu in ords) {
-        p <- TSA::periodogram(ts[1:lu], plot = F)
+        p <<- TSA::periodogram(ts[1:lu], plot = F)
         dds <- data.frame(freq = 1/p$freq, spec = p$spec, orden = 1:length(p$spec))
+        print(-dds$spec)
         dds <- head(dds[order(-dds$spec), ], place)
         ddT <- rbind(ddT, dds)
     }
+
     ddT <- ddT[order(-ddT$spec), ]
     Maxi <- max(ddT$spec)
     ddT <- head(ddT[ddT$orden > 2, ], 15)
