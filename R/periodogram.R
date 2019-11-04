ba70b09038321559fc2cd17fc98d58d0fb994535
José Carlos <30965492+jcval94@users.noreply.github.com>
2019-11-04 11:13:59 +0000

    description

--- R/periodogram.R
+++ R/periodogram.R
@@ -1,6 +1,6 @@
-#' Applies periodogram function for different chunks of a time series to get
+#' Compute periodogram function for different chunks of a time series to get the top
 #'
-#' @param ts time series object
+#' @param ts A multivariate time series
 #' @param place
 #'
 #' @return
@@ -19,22 +19,26 @@
 #' periodogram(df, place = 10)
 #'
 #'
-periodogram <- function(ts, place = 10) {
-    ddT <- data.frame(freq = c(), spec = c(), orden = c())
-    ords <- floor(length(ts) * 0.7):length(ts)
-    for (lu in ords) {
-        p <<- TSA::periodogram(ts[1:lu], plot = F)
-        dds <- data.frame(freq = 1/p$freq, spec = p$spec, orden = 1:length(p$spec))
-        print(-dds$spec)
-        dds <- head(dds[order(-dds$spec), ], place)
-        ddT <- rbind(ddT, dds)
+periodogram <- function(t, place = 10) {
+    dts<-dim(t)
+    for(i in 1:(dts[2])){
+        ts<-t[,i]
+        ddT <- data.frame(freq = c(), spec = c(), orden = c())
+        ords <- floor(length(ts) * 0.7):length(ts)
+        for (lu in ords) {
+            p <- TSA::periodogram(ts[1:lu], plot = F)
+            dds <- data.frame(freq = 1/p$freq, spec = p$spec, orden = 1:length(p$spec))
+            dds <- head(dds[order(-dds$spec), ], place)
+            ddT <- rbind(ddT, dds)
+        }
+        ddT <- ddT[order(-ddT$spec), ]
+        Maxi <- max(ddT$spec)
+        ddT <- head(ddT[ddT$orden > 2, ], 15)
+        ddT$Freq_Orden <- paste0(ddT$freq, "_", ddT$orden)
+        ddT <- suppressWarnings(reshape2::dcast(ddT, Freq_Orden ~ ., max, value.var = "spec"))
+        ddT$. <- ddT$./Maxi
+        ddT <- ddT[order(-ddT$.), ]
+        listn[[i]]<-list(unique(as.numeric(do.call("rbind", strsplit(ddT$Freq_Orden, "_")))), ddT)
     }
-    ddT <- ddT[order(-ddT$spec), ]
-    Maxi <- max(ddT$spec)
-    ddT <- head(ddT[ddT$orden > 2, ], 15)
-    ddT$Freq_Orden <- paste0(ddT$freq, "_", ddT$orden)
-    ddT <- suppressWarnings(reshape2::dcast(ddT, Freq_Orden ~ ., max, value.var = "spec"))
-    ddT$. <- ddT$./Maxi
-    ddT <- ddT[order(-ddT$.), ]
-    return(list(unique(as.numeric(do.call("rbind", strsplit(ddT$Freq_Orden, "_"))[, 1])), ddT))
+    return()
 }
