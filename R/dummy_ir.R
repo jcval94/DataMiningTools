3732c82a6c44b8190fb2b503bd8a19e03b3c16a5
José Carlos <30965492+jcval94@users.noreply.github.com>
2019-11-27 16:36:19 -0600

    Structural change

--- R/dummy_ir.R
+++ R/dummy_ir.R
@@ -11,7 +11,15 @@
 #'
 dummy_ir<-function(X){
   if("data.frame" %in% class(X)){
-    return(sapply(X,dummy_ir(X)[[2]]))
+    Cols<-(lapply(X,dummy_ir))
+    cuts<-list()
+    for(cl in 1:length(Cols)){
+      clm<-Cols[[cl]][[2]]
+      cuts[[cl]]<-Cols[[cl]][[1]]
+      if(!is.numeric(clm)){next()}
+      X[[paste0(names(Cols)[cl],"_ir")]]<-clm
+    }
+    return(list(X,cuts))
   }else{
     if(is.numeric(X)){
       IR<-quantile(X)
@@ -19,6 +27,8 @@ dummy_ir<-function(X){
       Inter_Q<-c(IR[2]-IR_diff,IR[4]+IR_diff)
       X_OL<-ifelse(X< Inter_Q[1] | X> Inter_Q[2],1,0)
       return(list(Inter_Q,X_OL))
+    }else{
+      return(list(NA,X))
     }
   }
 }
