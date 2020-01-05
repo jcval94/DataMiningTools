2907769dbdb5e91d7c05dd68784e8b1276778bca
Carlos xD <jcval94@gmail.com>
2020-01-05 00:08:04 -0600

    Optimized fun

--- R/replacemet.R
+++ R/replacemet.R
@@ -3,6 +3,8 @@
 #' @param X numeric vector
 #' @param values values to replace
 #' @param limits numeric vector, contain the upper limits of each interval to return values vector
+#' @param include_all FALSE, if TRUE, changes the low limit to -Inf and the upper value to Inf
+#'
 #'
 #' @return
 #' @export
@@ -19,15 +21,29 @@
 #' replacemet(10, values, limits)
 #'
 #'
-#' X <- c(1.5, 3, 6, 14)
+#' X <- c(0, 3, 6, 14)
 #' replacemet(X, values, limits)
 #'
 #'
-replacemet <- function(X, values, limits) {
-  a <- c()
-  for (i in 1:(length(limits) + 1)) {
-    a <- ifelse(X < limits[i + 1] & X >= limits[i], values[i], NaN)
-    ifelse(is.na(a), a <- ifelse(X < limits[i + 1] & X >= limits[i], values[i], NaN), (break)())
+replacemet <- function(X, values, limits, include_all=FALSE) {
+  lv<-length(values)
+  ll<-length(limits)
+  lx<-length(X)
+  if(lv!=(ll-1)){
+    warning("limits must have the same length that values + 1")
+    return(invisible())
+  }
+  if(include_all){
+    limits[1]<- -Inf
+    limits[ll]<- Inf
+  }
+  a <- rep(NA,lx)
+  for (i in 1:(ll - 1)) {
+    cond<-X < limits[i + 1] & X >= limits[i]
+    a[cond] <- values[i]
+    if(!any(is.na(a))){
+      break()
+    }
   }
   return(a)
 }
