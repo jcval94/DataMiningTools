086d2207a2f118dd0149b3ed139dff5aeae91324
Carlos xD <jcval94@gmail.com>
2020-01-04 23:40:50 -0600

    test

--- R/replacemet.R
+++ R/replacemet.R
@@ -1,33 +1,33 @@
 #' Replace values in a numeric vector
 #'
-#' @param X
-#' @param valores
-#' @param cortes
+#' @param X numeric vector
+#' @param values values to replace
+#' @param limits numeric vector, contain the upper limits of each interval to return values vector
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #'
-#' proporciones <- c(4, 2)
-#' valores <- c(4, 8)
-#' cortes <- c(1, 5, 10)
+#' values <- c(4, 8)
+#' limits <- c(1, 5, 10)
 #'
-#' replacemet(1, valores, cortes)
+#' replacemet(1, values, limits)
 #'
-#' replacemet(5, valores, cortes)
+#' replacemet(5, values, limits)
 #'
-#' replacemet(10, valores, cortes)
-#'
-#' replacemet(.1, valores, cortes)
+#' replacemet(10, values, limits)
 #'
 #'
+#' X <- c(1.5, 3, 6, 14)
+#' replacemet(X, values, limits)
 #'
-replacemet <- function(X, valores, cortes) {
+#'
+replacemet <- function(X, values, limits) {
   a <- c()
-  for (i in 1:(length(cortes) + 1)) {
-    a <- ifelse(X < cortes[i + 1] & X >= cortes[i], valores[i], NaN)
-    ifelse(is.na(a), a <- ifelse(X < cortes[i + 1] & X >= cortes[i], valores[i], NaN), (break)())
+  for (i in 1:(length(limits) + 1)) {
+    a <- ifelse(X < limits[i + 1] & X >= limits[i], values[i], NaN)
+    ifelse(is.na(a), a <- ifelse(X < limits[i + 1] & X >= limits[i], values[i], NaN), (break)())
   }
   return(a)
 }
